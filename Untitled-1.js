9e7de66ad4dbb3053b9d677d45e5aea54eb66884
el-masry115 <m.o01154991946@gmail.com>
2022-05-11 01:28:13 +0300

    pp

---
+++ Untitled-1.js
@@ -0,0 +1,137 @@
+
+
+// var number1 = window .prompt("enter number1")
+// var number2 = window .prompt("enter number2")
+
+// var results = Number (number1) + Number (number2)
+
+// console .log("plus" +  results) 
+//  number99 = window .prompt("ثغبلهغف")
+// document .getElementById ('dweo').innerHTML="welcom mohmed"
+
+// var userName ="username";
+// console.log (typeof userName);
+// var Age = 6;
+// console.log( typeof Age);
+// var jhu;
+// console.log(typeof jhu);
+// var C= null
+// console.log(typeof C);
+// var HH = 6;
+// var LL = 6;
+// console.log(HH+LL);
+// var JJ="JKHG"
+// var II=55
+// console.log(typeof II+ Number (JJ))
+
+// var number1=window .prompt("number1")
+// var number2=window .prompt("number2")
+// console.log(number1+number1)
+
+// console.log ( Number("55")+II+ Number("66"))
+
+// // 
+// // 
+// // 
+// // 
+// var userNamber= Number(window.prompt("user namber"))
+// if (userNamber>=0){
+//     console.log("possve")
+// }
+// else{
+//     console.log("nagative")
+// }
+
+
+//  var userNamber2= Number(window.prompt("user namber2"))
+//  switch (userNamber2) {
+//      case "admine":
+//          console.log("sdfg");
+//         break;
+
+//      case "leader": 
+//          console.log("ilks68re");
+//          break;
+         
+//          default:
+//              console.log("dsklj;lsdguytfuytf");
+         
+
+    
+//  }
+// var role= Number( window.prompt("dygjj"))
+// switch (role){
+//     case "leader":
+//         console.log("Fzd")
+//     default:
+//         console.log("fzdf")
+// }
+// var role ="a"
+
+// for ( i=0; i<10; i++ ){
+//     role= role+"m"
+//     console.log(role)
+// }
+
+
+
+// var myFrinds=["Ahmed", "Ali", "mohmmed", "lll"];
+
+
+// for  (i=0 ; i< myFrinds.length; i++){
+//     console.log(myFrinds[i])
+// }
+
+
+// var role1= Number( window.prompt("dygjj"))
+// var role2= Number( window.prompt("dygjj"))
+// function dd(x+y){
+//     return x+y
+// }
+// function ff(xf+yd){
+// console.log(xf/yd)
+// }
+// var hj =(role1+role2)
+// ff(hj,2)
+// var nameuser =document.getElementById("gf")
+// function welcome(){
+//     document.getElementById("dweo").innerHTML=nameuser.value
+// }
+// var ff=22
+
+// function calc() {
+//     console.log(ff);
+
+    
+// }
+// calc()
+
+// if (gg=45){
+//     var gg=45
+
+// }
+// console.log(gg);
+
+// ( function (){
+//     var JJ=22
+//     JJ++
+//     console.log(JJ);
+
+// })()
+
+// ex-Avg
+
+function sum(x,y){
+    console.log(x+y);
+
+}
+function calcAvge(k){
+    console.log(k/2)
+}
+var Num1=Number(window.prompt("number1"))
+var Num2=Number(window.prompt("number1"))
+
+var result= sum(Num1,Num2)
+calcAvge(26)
+
+
---
+++ hom.js
@@ -0,0 +1,420 @@
+// // ==============starrt ques-9==============
+// // ==============starrt ques-9============== -->
+
+// var Namber_9=Number(window.prompt("quws-9 enter your namder"))
+// for(i=1;i<=Namber_9;i++){
+//     console.log(i)
+// }
+
+// // <!-- ==============end ques-9==============
+// // ==============end ques-9==============
+
+
+// // ==============start ques-10==============
+// // ==============start ques-10============== -->
+// var Namber_10=Number(window.prompt("quws-10 enter your namder"))
+// for(i=1;i<=12;i++){
+//     console.log(Namber_10*[i])
+// }
+
+// // <!-- ==============end ques-10==============
+// // ==============end ques-10==============
+
+
+// // ==============start ques-11==============
+// // ==============start ques-11============== -->
+// var Namber_11=Number(window.prompt("quws-11 enter your namder"))
+// for(i=2;i<=Namber_11;i+=2){
+//     console.log(i)
+// }
+
+// // <!-- ==============end ques-11==============
+// // ==============end ques-11==============
+
+
+
+// // ==============start ques-12==============
+// // ==============start ques-12============== -->
+
+// var Namber_12_1=Number(window.prompt("quws-12-1 enter your namder"))
+// var Namber_12_2=Number(window.prompt("quws-12-2 enter your namder"))
+// console.log(Namber_12_1**Namber_12_2)
+
+// // <!-- ==============end ques-12==============
+// // ==============end ques-12============== -->
+
+
+// // <!-- 
+// // ==============start ques-13==============
+// // ==============start ques-13============== -->
+
+// var Namber_13_1=Number(window.prompt("quws-13-1 enter your namder"))
+// var Namber_13_2=Number(window.prompt("quws-13-2 enter your namder"))
+// var Namber_13_3=Number(window.prompt("quws-13-3 enter your namder"))
+// var Namber_13_4=Number(window.prompt("quws-13-4 enter your namder"))
+// var Namber_13_5=Number(window.prompt("quws-13-5 enter your namder"))
+// var total=(Namber_13_1+Namber_13_2+Namber_13_3+Namber_13_4+Namber_13_5)
+// console.log(total)
+// console.log(total / 5)
+// // console.log(total / 5)
+// if (total<=500){
+//     console.log(total)
+//     console.log(total/500*100)
+// }
+// else{
+//    " marck not corriect"
+// }
+
+
+
+// // <!-- ==============end ques-13==============
+// // ==============end ques-13==============
+
+
+// // ==============start ques-14==============
+// // ==============start ques-14==============
+
+// var Namber_14_1=Number(window.prompt("ques-13-1 Enter the physics degree(200)"))
+// var Namber_14_2=Number(window.prompt("ques-13-2 Enter the degree of chemistry(200)"))
+// var Namber_14_3=Number(window.prompt("ques-13-3 Enter the degree of life (200)"))
+// var Namber_14_4=Number(window.prompt("ques-13-4 Enter computer grader (200)"))
+// var Namber_14_5=Number(window.prompt("ques-13-5 enter your namder (200)"))
+// var total=(Namber_14_1+Namber_14_2+Namber_14_3+Namber_14_4+Namber_14_5)
+// if(Namber_14_1<=200){
+//     console .log(Namber_14_1)
+//     console.log(Namber_14_1/200*100+"%")
+
+
+// }
+// else{
+//     console.log("wrong_degree")
+// }
+
+// if(Namber_14_2<=200){
+//     console .log(Namber_14_2)
+//     console.log(Namber_14_2/200*100+"%")
+
+
+// }
+// else{
+//     console.log("wrong_degree")
+// }
+
+// if(Namber_14_3<=200){
+//     console .log(Namber_14_3)
+//     console.log(Namber_14_3/200*100 +"%")
+
+
+// }
+// else{
+//     console.log("wrong_degree")
+// }
+
+// if(Namber_14_4<=200){
+//     console .log(Namber_14_4)
+//     console.log(Namber_14_4/200*100+"%")
+
+
+// }
+// else{
+//     console.log("wrong_degree")
+// }
+// if(Namber_14_5<=200){
+//     console .log(Namber_14_5)
+//     console.log(Namber_14_5/200*100 +"%")
+// }
+// else{
+//     console.log("wrong_degree")
+// }
+ 
+// // ==============end ques-14==============
+// // ==============end ques-14==============
+
+
+
+// // ==============start ques-15==============
+// // ==============start ques-15==============
+
+// var Namber_15=window.prompt("ques-15 Enter the name of the month")
+// switch(Namber_15){
+//     case "January":
+//     console.log(30)
+//     break;
+//     case "February":
+//     console.log(29)
+//     break;
+//     case "March":
+//     console.log(30)
+//     break;
+//     case "April":
+//     console.log(31)
+//     break;
+//     case "mayo":
+//     console.log(29)
+//     break;
+//     case "June":
+//     console.log(30)
+//     break;
+//     case "July":
+//     console.log(30)
+//     break;
+//     case "August":
+//     console.log(30)
+//     break;
+//     case "September":
+//     console.log(30)
+//     break;
+//     case "October":
+//     console.log(30)
+//     break;
+//     case "November":
+//     console.log(29)
+//     break;
+//     case "December":
+//     console.log(28)
+//     break;
+//     default:
+//     console.log("wrong month")
+//     break;
+// }
+// // ==============end ques-15==============
+// // ==============end ques-15============== -->
+
+
+
+
+// // ==============start ques-16============== -->
+// // ==============start ques-16============== -->
+
+
+// var Namber_16=window.prompt("ques-16 Enter the vowel or consonant");
+// switch(Namber_16){
+//     case("i"):
+//     console.log("vowel");
+//     break;
+//     case("e"):
+//     console.log("vowel");
+//     break;
+//     case("o"):
+//     console.log("vowel");
+//     break;
+//     case("u"):
+//     console.log("vowel");
+//     break;
+//     case("a"):
+//     console.log("vowel");
+//     break;
+//     default:
+//     console.log("Consonant");
+// }
+
+
+// // ==============end ques-16============== -->
+// // ==============end ques-16============== -->
+
+
+
+// // ==============start ques-17============== -->
+// // ==============start ques-17============== -->
+
+// var Question_17_1=Number(window.prompt("ques-17-1 Enter a number")) 
+// var Question_17_2=Number(window.prompt("ques-17-2 Enter a number")) 
+
+// switch(Question_17_1) {
+//     case (Question_17_1):
+//         if(Question_17_1>Question_17_2){
+//             console.log(Question_17_1)
+            
+
+//         }
+//        else{
+//         console.log(Question_17_2)
+
+//        }
+
+// }
+
+
+
+// // ==============end ques-17============== -->
+// // ==============end ques-17============== -->
+
+
+
+
+// // ==============start ques-18============== -->
+// // ==============srart ques-18============== -->
+
+// var Question_18=Number(window.prompt("ques-18 Enter a number")) 
+
+// switch(Question_18) {
+//     case (Question_18):
+//         if(Question_18%2==0){
+//             console.log(" الرقم زوجي")
+//         }
+//         else if(Question_18%2==1){
+//         console.log("الرقم فردي")
+//         }
+//         else if (Question_18 %2!=0 && Question_18 %2!=1){
+//             console.log("The figure is incorrect")
+//         }
+// }
+
+
+// // ==============end ques-18============== -->
+// // ==============end ques-18============== -->
+
+
+
+
+
+// // ==============start ques-19============== -->
+// // ==============start ques-19============== -->
+
+// var Question_19=Number(window.prompt("ques-19 Enter a number")) 
+
+// switch(Question_19) {
+//     case (Question_19):
+//         if(Question_19>=0){
+//             console.log(" The number is positive")
+//         }
+//         else{
+//             console.log("The number is negative")
+//         }
+       
+// }
+
+
+// // ==============end ques-19============== -->
+// // ==============end ques-19============== -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var btn1=document.getElementById("btn1").value;
+// function btn_1(){
+//     viwe=btn1 +viwe
+//     document.getElementById("viwe").value=viwe
+// }
+// var btn2=document.getElementById("btn2").value;
+// function btn_2(){
+//     viwe=btn2 +viwe
+//     document.getElementById("viwe").value=viwe
+// }
+// var btn3=document.getElementById("btn3").value;
+// function btn_3(){
+//     viwe=btn3 +viwe
+//     document.getElementById("viwe").value=viwe
+// }
+// var btn4=document.getElementById("btn4").value;
+// function btn_4(){
+//     viwe=btn4 +viwe
+//     document.getElementById("viwe").value=viwe
+// }
+// var btn5=document.getElementById("btn5").value;
+// function btn_5(){
+//     viwe=btn5 +viwe
+//     document.getElementById("viwe").value=viwe
+// }
+// var btn6=document.getElementById("btn6").value;
+// function btn_6(){
+//     viwe=btn6 +viwe
+//     document.getElementById("viwe").value=viwe
+// }
+// var btn7=document.getElementById("btn7").value;
+// function btn_7(){
+//     viwe=btn7 +viwe
+//     document.getElementById("viwe").value=viwe
+// }
+// var btn8=document.getElementById("btn8").value;
+// function btn_8(){
+//     viwe=btn8 +viwe
+//     document.getElementById("viwe").value=viwe
+// }
+// var btn9=document.getElementById("btn9").value;
+// function btn_9(){
+//     viwe=btn9 +viwe
+//     document.getElementById("viwe").value=viwe
+// }
+// var btn0=document.getElementById("btn0").value;
+// function btn_0(){
+//     viwe=btn0 +viwe
+//     document.getElementById("viwe").value=viwe
+// }
+
+
+
+
+// var btnbls=document.getElementById("btnbls").value;
+// function btn_bls(){
+//     viwe=btnbls +viwe
+//     document.getElementById("viwe").value=viwe
+// }
+// var btnmin=document.getElementById("btnmin").value;
+// function btn_min(){
+//     viwe=btnmin +viwe
+//     document.getElementById("viwe").value=viwe
+// }
+// var btnmult=document.getElementById("btnmult").value;
+// function btn_mult(){
+//     viwe=btnmult +viwe
+//     document.getElementById("viwe").value=viwe
+// }
+// var btndot=document.getElementById("btndot").value;
+// function btn_dot(){
+//     viwe=btndot +viwe
+//     document.getElementById("viwe").value=viwe
+// }
+// var btneq=document.getElementById("btneq").value;
+// function btn_eq(){
+//     // viwe=btneq +viwe
+//     document.getElementById("viwe").value=eval(viwe);
+// }
+// var btndiv=document.getElementById("btndiv").value;
+// function btn_div(){
+//     viwe=btndiv +viwe
+//     document.getElementById("viwe").value=viwe
+// }
+// var viwe=document.getElementById("viwe").value;
+
+
+
+
+
+
+
+// // ==============start ques-20============== -->
+// // ==============start ques-20============== -->
+
+
+
+// // ==============end ques-20============== -->
+// // ==============end ques-20============== -->
+
+
